54e337f34d061bf70258d380da7300621aac5b4f
Gena888 <theGena888@gmail.com>
2021-01-31 21:03:38 +0300

    feat: test ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    let sortedArr = array.sort(function (a, b) {
+      return a - b;
+    });
+    return sortedArr[0]
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    let sortedArr = array.sort(function (a, b) {
+      return b - a;
+    });
+    return sortedArr[0]
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array == undefined || array.length == 0) {
+    return 0;
+  } else {
+    let result = 0
+    for (let i = 0; i <= array.length - 1; i++) {
+      result += array[i]
+    }
+    return result/array.length;
+  }
 }
